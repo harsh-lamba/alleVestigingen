a5dbb69813206636562ec07f7c12a663d35536f2
harsh18 <harsh.lamba181189@gmail.com>
2016-06-23 00:13:21 +0530

    Mockup js file with graphs

--- js/mockup.js
+++ js/mockup.js
@@ -4,7 +4,9 @@
 	app.google.charts.load('current', {'packages':['corechart', 'bar']});
     app.google.charts.setOnLoadCallback(drawPieChart);
 
-    app.google.charts.setOnLoadCallback(drawStackedBarChart);
+    app.google.charts.setOnLoadCallback(drawStackedBarChart1);
+    app.google.charts.setOnLoadCallback(drawStackedBarChart2);
+    app.google.charts.setOnLoadCallback(drawStackedBarChart3);
 
     function drawPieChart() {
 
@@ -26,48 +28,104 @@
         var chart = new google.visualization.PieChart(document.getElementById('piechart'));
 
         chart.draw(data, options);
-      }
+    }
 
-	function drawStackedBarChart() {
-		var data = new google.visualization.arrayToDataTable([
-		  ['Galaxy', 'Distance', 'Brightness'],
-		  ['Canis Major Dwarf', 8000, 23.3],
-		  ['Sagittarius Dwarf', 24000, 4.5],
-		  ['Ursa Major II Dwarf', 30000, 14.3],
-		  ['Lg. Magellanic Cloud', 50000, 0.9],
-		  ['Bootes I', 60000, 13.1]
-		]);
+	function drawStackedBarChart1() {
+		var data = google.visualization.arrayToDataTable([
+	        [' ', 'Value', 'Value in %'],
+	        ['Passende afgeronde kandidaten', 8175000, 8008000],
+	        ['Gesprek ingepland', 3792000, 3694000],
+	        ['Gesprek plaats gevonden', 2695000, 2896000],
+	        ['Neit opgevolgd', 2099000, 1953000]
+	      ]);
 
 		var options = {
-		  bars: 'horizontal', // Required for Material Bar Charts.
-		  legend : {position:'none'},
-		  series: {
-		    0: { axis: 'distance' }, // Bind series 0 to an axis named 'distance'.
-		    1: { axis: 'brightness' } // Bind series 1 to an axis named 'brightness'.
-		  },
-		  axes: {
-		    x: {
-		      distance: {titlePosition:'none'}, // Bottom x-axis.
-		      brightness: {titlePosition:'none'} // Top x-axis.
-		    }
-		  },
-		  hAxis : {
-		  	gridlines : {count:-1},
-		  	textPosition:'none',
-		  	titlePosition:'none'
-		  },
-		  vAxis : {
-		  	gridlines : {count:-1},
-		  	textPosition:'none',
-		  	titlePosition:'none'
-		  }
+			chart: {
+			//title: 'Population of Largest U.S. Cities'
+			},
+			hAxis: {
+				title: 'Total Population',
+				titlePosition : 'none',
+				minValue: 0,
+			},
+			vAxis: {
+				title: 'City'
+			},
+			bars: 'horizontal',
+			legend : {position:'none'}
 		};
 
 		var chart1 = new google.charts.Bar(document.getElementById('barchart1'));
 		//var chart2 = new google.charts.Bar(document.getElementById('barchart2'));
 		//var chart2 = new google.charts.Bar(document.getElementById('barchart3'));
-		//chart1.draw(data, options);
+		chart1.draw(data, options);
 		//chart2.draw(data, options);
 		//chart3.draw(data, options);
+	};
+	function drawStackedBarChart2() {
+		var data = google.visualization.arrayToDataTable([
+	        [' ', 'Value', 'Value in %'],
+	        ['Gesproken kandidaten', 8175000, 8008000],
+	        ['Kandidaat voorgesteld bij klant', 3792000, 3694000],
+	        ['Sollicitatiegesprek ingepland', 2695000, 2896000],
+	        ['Sollicitatiegesprek plaatsgevonden', 2099000, 1953000],
+	        ['Niet opgevolgd', 2099000, 1953000],
+	        ['Kandidaat geplaatst', 2099000, 1953000]
+	      ]);
+
+		var options = {
+			chart: {
+			//title: 'Population of Largest U.S. Cities'
+			},
+			hAxis: {
+				title: 'Total Population',
+				titlePosition : 'none',
+				minValue: 0,
+			},
+			vAxis: {
+				title: 'City'
+			},
+			bars: 'horizontal',
+			legend : {position:'none'}
+		};
+
+		//var chart1 = new google.charts.Bar(document.getElementById('barchart1'));
+		var chart2 = new google.charts.Bar(document.getElementById('barchart2'));
+		//var chart2 = new google.charts.Bar(document.getElementById('barchart3'));
+		//chart1.draw(data, options);
+		chart2.draw(data, options);
+		//chart3.draw(data, options);
+	};
+	function drawStackedBarChart3() {
+		var data = google.visualization.arrayToDataTable([
+	        [' ', 'Value', 'Value in %'],
+	        ['1e week', 8175000, 8008000],
+	        ['1e maand', 3792000, 3694000],
+	        ['3 maanden', 2695000, 2896000],
+	        ['3 maandelijks', 2099000, 1953000]
+	      ]);
+
+		var options = {
+			chart: {
+			//title: 'Population of Largest U.S. Cities'
+			},
+			hAxis: {
+				title: 'Total Population',
+				titlePosition : 'none',
+				minValue: 0,
+			},
+			vAxis: {
+				title: 'City'
+			},
+			bars: 'horizontal',
+			legend : {position:'none'}
+		};
+
+		//var chart1 = new google.charts.Bar(document.getElementById('barchart1'));
+		//var chart2 = new google.charts.Bar(document.getElementById('barchart2'));
+		var chart3 = new google.charts.Bar(document.getElementById('barchart3'));
+		//chart1.draw(data, options);
+		//chart2.draw(data, options);
+		chart3.draw(data, options);
 	};
 })(window);
